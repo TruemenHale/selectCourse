aa48613dd8aec2a723487c2d6f9211b7bcc9f571
TruemenHale <truemenhale@Hales-MacBook-Pro.local>
2015-05-30 09:13:58 +0800

    Login

--- js/CourseList.js
+++ js/CourseList.js
@@ -6,13 +6,25 @@ $(function(){
 	var oClose=$('.close');
 	var oM=$('.mask');
 	var aT=$('.Teacher');
+	var oL=$('.login_box');
+	var oC=$('.login_close');
+	var oLb=$('.login_btn');
 	aT.click(function(){
 		oTi.css('display','block');
+		oM.css('display','block');
+	});
+	oLb.click(function(){
+		oL.css('display','block');
 		oM.css('display','block');
 	});
 	oClose.click(function(){
 		oTi.css('display','none');
 		oM.css('display','none');
 	});
+	oC.click(function(){
+		oL.css('display','none');
+		oM.css('display','none');
+	});
 	oTi.css('left',($(window).width()-1000)/2);
+	oL.css('left',($(window).width()-507)/2);
 });
