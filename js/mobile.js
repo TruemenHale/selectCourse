59735a1b9973de01b117407ff3bb47dddf61a14f
TruemenHale <truemenhale@Hales-MacBook-Pro.local>
2015-06-15 11:34:38 +0800

    886

---
+++ js/mobile.js
@@ -0,0 +1,9 @@
+/**
+ * Created by truemenhale on 15/6/9.
+ */
+$(function(){
+	var oHeader=$('.header');
+	var oTitle=$('.title');
+	oHeader.css('height',$(window).width()*0.1333333);
+	oTitle.css('line-height',$(window).width()*0.1333333+'px');
+});
